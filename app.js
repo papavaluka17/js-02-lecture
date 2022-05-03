a0001f0064c7bcf2e145c9b6b5259d42f86e2ab0
papavaluka17 <96058260+papavaluka17@users.noreply.github.com>
2022-05-03 18:03:55 +0400

    homework

---
+++ app.js
@@ -0,0 +1,21 @@
+// დავალება1
+let arr1 = [1, 3, 8, 21, 765, 90, 764, 2323, 765,]
+console.log(Math.max(...arr1))
+console.log(Math.min(...arr1))
+
+
+// დავალება2
+let number1 = Math.pow(5,3);
+console.log(number1);
+
+let number2 = Math.pow(4, 4);
+console.log(number2);
+
+// დავალება3
+let arrow = (a, b) => {
+    let arrFunc = a ** b;
+    return arrFunc;
+}
+
+let result = arrow(5, 2);
+console.log(result);
